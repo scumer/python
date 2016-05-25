8f97fe514b7db3d384eabad4020407cab80d7001
xu hui <xuhui_2010@126.com>
2016-05-25 09:56:12 +0800

    aoi

--- serial/auto_aoi.py
+++ serial/auto_aoi.py
@@ -1,1 +1,24 @@
 # coding=utf-8
+
+import serial
+import binascii
+
+d_check = '010300000001840a'
+d_reply = '01030200017984'
+
+port = serial.Serial('COM1')
+
+while True:
+    if port.inWaiting() > 6:
+        data = port.read_all()
+        hdata = binascii.b2a_hex(data)
+        print hdata
+
+        if hdata == d_check:
+            port.write(binascii.a2b_hex(d_reply))
+        else:
+            port.write(data)
+
+
+port.close()
+
